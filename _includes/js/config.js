d38c4852f2be5541e7d82ca1fd4f90a4e9a33fcf
CaroleSinou <32440327+CaroleSinou@users.noreply.github.com>
2023-09-20 15:50:21 -0400

    Update config.js

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -2,9 +2,9 @@ var siteConfig = {
   version: 2,
   routes: {
     enabledRoutes: ['occurrenceSearch', 'collectionSearch', 'collectionKey', 'institutionSearch', 'institutionKey', 'datasetKey', 'datasetSearch', 'literature'], // what widgets do you include on your site. If not included we will link to gbif.org (for showing individual datasets for example)
-    occurrenceSearch: { // you can overwrite individual routes. 
-      route: '/occurrence/search' // in this case we want the occurrence search to be available on a url that says specimens instead
-    }
+    //occurrenceSearch: { // you can overwrite individual routes. 
+      //route: '/occurrence/search' // in this case we want the occurrence search to be available on a url that says specimens instead
+    //}
   },
   availableCatalogues: ['INSTITUTION', 'COLLECTION', 'OCCURRENCE', 'DATASET'],
   occurrence: {
@@ -62,17 +62,17 @@ var siteConfig = {
     //maptiler: "GET_YOUR_OWN_TOKEN", // https://github.com/gbif/hosted-portals/issues/229
     //mapbox: "GET_YOUR_OWN__TOKEN"
   //},
-  //maps: {
+  maps: {
     //locale: 'ja', // we want to show the maps in japanese
-    //defaultProjection: 'MERCATOR',
-    //defaultMapStyle: 'BRIGHT',
-    //mapStyles: {
-      //ARCTIC: ['NATURAL', 'BRIGHT'],
-      //PLATE_CAREE: ['NATURAL', 'BRIGHT', 'DARK'],
-      //MERCATOR: ['NATURAL', 'BRIGHT', 'SATELLITE', 'DARK'],
-      //ANTARCTIC: ['NATURAL', 'BRIGHT', 'DARK']
-    //}
-  //},
+    defaultProjection: 'MERCATOR',
+    defaultMapStyle: 'BRIGHT',
+    mapStyles: {
+      ARCTIC: ['NATURAL', 'BRIGHT'],
+      PLATE_CAREE: ['NATURAL', 'BRIGHT', 'DARK'],
+      MERCATOR: ['NATURAL', 'BRIGHT', 'SATELLITE', 'DARK'],
+      ANTARCTIC: ['NATURAL', 'BRIGHT', 'DARK']
+    }
+  },
   messages: { // custom overwrites for the translations, e.g. label the occurrence catalog as a specimen catalog to match our data scope of specimens.
     "catalogues.occurrences": "Occurrences"
   }
